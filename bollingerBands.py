dc4a24558d32e0ad464eafc2ff3db1209e577912
Kenan Sooklall <ksooklall100@gmail.com>
2016-11-17 12:46:11 -0500

    Added flexibility

--- bollingerBands.py
+++ bollingerBands.py
@@ -1,4 +1,4 @@
-""" Technical analysis of the S&P500 index"""
+""" Appling standard devitation analysis to stock prices"""
 
 import pandas as pd
 import numpy as np
@@ -6,54 +6,65 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from pandas.io.data import DataReader
 import datetime
-
+plt.style.use('ggplot')
 # S&p500: ^GSPC
 
+# Select trading range for analysis
 start = datetime.datetime(2016,1,1)
-end = datetime.datetime(2016,9,1)
+end = datetime.datetime(2016,11,16)
+
+# Select stock, window and band length
 stock = 'FSLR'
-df = DataReader(stock,'yahoo',start,end)
+window = 20
+band_length = 2
 
-df['Date'] = df.index
-df['Date'] = pd.to_datetime(df['Date'])
-df['Date_delta'] = (df['Date']-df['Date'].min())/np.timedelta64(1,'D')
+# Collect data from yahoo
+df = DataReader(stock,'yahoo',start)
 
-mean = pd.rolling_mean(df['Adj Close'],20)
-std = pd.rolling_std(df['Adj Close'],20)
-upperBand = mean+2*std
-lowerBand = mean-2*std
-"""
+# Calculate rolling mean of Adjust close
+mean = pd.rolling_mean(df['Adj Close'],window)
+# Calculate rolling standard devitation
+std = pd.rolling_std(df['Adj Close'],window)
+# Determine upper and lower bands
+upperBand = mean+band_length*std
+lowerBand = mean-band_length*std
+
+# Buying analysis
 if df['Adj Close'][-1]<lowerBand[-1]:
     print('Buy')
 elif df['Adj Close'][-1]>upperBand[-1]:
     print('Sell')
 else:
-    print('Hold if owned')"""
+    print('Hold if owned')
 
+# Calculate rolling mean of Daily Returns
 df['Daily Returns'] = df['Adj Close']/df['Adj Close'].shift()-1
-dmean = pd.rolling_mean(df['Daily Returns'],20)
-dstd = pd.rolling_std(df['Daily Returns'],20)
-dupperBand = dmean+2*dstd
-dlowerBand = dmean-2*dstd
+dmean = pd.rolling_mean(df['Daily Returns'],window)
+dstd = pd.rolling_std(df['Daily Returns'],window)
+dupperBand = dmean+band_length*dstd
+dlowerBand = dmean-band_length*dstd
 
-# plotting
+# p
+datemin = datetime.datetime(2016,start.month+1,1)
+datemax = end
 name = 'Adj Close'
 plt.figure(figsize=(20,20))
 plt.subplot(2,1,1)
 plt.plot(df[name],color='k', ls = 'solid')
-plt.plot(mean,color='r', label='Moving Average',linestyle = '-')
+plt.plot(mean,color='r', label='Moving Average',linestyle = '-', lineweight = 3)
 plt.plot(upperBand, color='b', label = 'Lower Band', linestyle = 'dotted', linewidth = 2)
 plt.plot(lowerBand, color='b', label = 'Upper Band', linestyle = 'dotted', lw = 3)
 plt.ylabel('Adj. Close')
+plt.xlim(datemin,datemax)
 plt.title(name)
 plt.legend(loc=1)
 
 plt.subplot(2,1,2)
 plt.plot(df['Daily Returns'], color='k')
-plt.plot(dmean,color='r', label='Moving Average',linestyle = '-')
+plt.plot(dmean,color='r', label='Moving Average',linestyle = '-', lineweight = 3)
 plt.plot(dupperBand, color='b', label = 'Lower Band', linestyle = 'dotted', linewidth = 2)
 plt.plot(dlowerBand, color='b', label = 'Upper Band', linestyle = 'dotted', lw = 3)
+plt.xlim(datemin,datemax)
 plt.ylabel('Adj. Close')
 plt.title(name)
-plt.legend(loc=2)
 plt.show()
