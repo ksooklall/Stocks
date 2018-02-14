8b47d0ff545a0a97140beeaa80ed419a2cbfaba6
ksooklall <ksooklall100@gmail.com>
2018-02-13 22:57:22 -0500

    finished default graph

--- RNN.py
+++ RNN.py
@@ -11,7 +11,7 @@ class rnn_network():
         self.window = window
 
     def get_batch(self):
-        # TO DO: Add feature to over lap days
+        # TODO: Add feature to over lap days
         start_index = len(self.serie) % self.window
 
         if not start_index:
@@ -19,6 +19,7 @@ class rnn_network():
         else:
             # If a multiple of window is not len(serie) chop off from the start
             batch = self.serie[start_index:].reshape(-1, self.window)
+        # TODO: Remove starting dates to get full batches
         X, y = [], []
         for i in range(0, len(batch), self.num_steps):
             if (i + self.num_steps+ 1) > len(batch):
@@ -26,28 +27,63 @@ class rnn_network():
             X.append(batch[i: i + self.num_steps])
             y.append(batch[i + self.num_steps])
 
-        return batch, np.array(X), np.array(y)
+        return (batch, np.array(X), np.array(y))
 
-    def tensors(self):
-        inputs = tf.placeholder([None, self.num_steps, self.window], dtype=tf.float32)
-        targets = tf.placeholder([None, self.window], dtype=tf.float32)
+    def get_train_test(self, split=0.1):
+        _, X, y = self.get_batch()
+        idx = int(np.round(len(X) * (1 - split)))
+        X_train, X_test, y_train, y_test = X[:idx], X[idx:], y[:idx], y[idx:]
+
+        X_train = np.expand_dims(X_train, 0)
+        
+        return (X_train, X_test, y_train, y_test)
+
+    def input_tensors(self):
+        inputs = tf.placeholder(dtype=tf.float32, shape=[None, self.window, self.num_steps])
+        targets = tf.placeholder(dtype=tf.float32, shape=[None, self.window])
         learning_rate = tf.placeholder(tf.float32, name='learning_rate')
-        return inputs, targets
+        return inputs, targets, learning_rate
         
     
     def rnn_cell(self, units, layers):
-        cell = tf.contrib.rnn.BasicLSTMCell(units)
+        cell = tf.nn.rnn_cell.BasicLSTMCell(units)
         cell = tf.contrib.rnn.MultiRNNCell([cell] * layers)
         initial_state = cell.zero_state(self.num_steps, dtype=tf.int32)
         return cell, initial_state
     
-    def model(self, units=32, layers=1):
+    def model(self, inputs, units=32, layers=1):
         cell, initial_state = self.rnn_cell(units, layers)
-        output, final_state = tf.nn.dynamic_rnn(cell, X, initial_state)
+        import pdb; pdb.set_trace()
+        output, final_state = tf.nn.dynamic_rnn(cell, inputs, initial_state)
         with tf.variable_scope('fc1'):
             fc1 = tf.contrib.full_connected(output, window, activation_fn=None)
-        return fc1
+        prediction = fc1
+        return prediction, final_state
 
-    def train(self):
-        inputs, targets = self.tensors()
-        pass
+    def default_graph(self):
+        with tf.Graph().as_default():
+            inputs, targets, learning_rate = self.input_tensors()
+            prediction, final_state = self.model(inputs)
+            optimizer = tf.tain.AdamOptimizer(learning_rate)
+            loss = tf.reduce_mean(tf.square(tf.subtract(prediction, targets)))
+            gradients = optimizer.compute_gradients(loss)
+            clipped_gradients = [tf.clip_by_value(grad, -1.0, 1.0) for grad, val in gradients]
+            train_optimizer = optimizer.apply_gradients(clipped_gradients)
+        return loss, train_optimizer
+
+    def batch_generator(X, y, batch_size=32):
+        for i in range(0, len(X), batch_size):
+            yield X[i:i + batch_size], y[i:i + batch_size]
+
+    def train(self, epochs, lr, verbose=5):
+        X_train, X_test, y_train, y_test = self.get_train_test()
+        loss, train_optimizer = self.default_graph()
+        # TODO: Add batching
+        with tf.Session() as sess:
+            for epoch in epochs:
+                for X, y in self.batch_generator(X_tain, y_train):
+                    loss, _ = sess.run([loss, train_optimizer], feed_dict={inputs: X, targets: y, learning_rate: lr})
+                if verbose and not (epoch % verbose):
+                    acc = sess.run([loss], feed_dict={inputs: X_valid, targets: y_valid})
+                    print('E:{:>3} Acc: {:.3f}'.format(epoch, acc))
+        return    
--- analysis.py
+++ analysis.py
@@ -4,10 +4,14 @@ import numpy as np
 from RNN import rnn_network
 import os
 
+
 if __name__ == '__main__':
+        EPOCH = 100
+        LEARNING_RATE = 0.001
+        
         master_df = pd.read_csv('E:/Analysis/sandp500/all_stocks_5yr.csv')
         df = master_df[master_df['Name'] == 'AMZN']
         serie = np.array(df['close'])
-        net = rnn_network(serie, 3, 5)
+        net = rnn_network(serie, 2, 5)
         batch, X, y = net.get_batch()
-        a = net.model()
+        net.train(EPOCH, LEARNING_RATE)
