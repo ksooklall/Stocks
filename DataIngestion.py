e34945cb3ebbec0057a900eb8c0bf0d4ec897f03
ksooklall <ksooklall100@gmail.com>
2018-08-19 22:47:08 -0400

    ccompleted sccraping of whispers

--- DataIngestion.py
+++ DataIngestion.py
@@ -15,6 +15,8 @@ raw_url - https://www.earningswhispers.com/stocks/fb
 
 Blue Chip Stocks
 'https://www.nasdaq.com/screening/companies-by-industry.aspx?sortname=marketcap&sorttype=1&exchange=NASDAQ'
+
+Use 8/15/2018 as test, small amount of stocks
 """
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
@@ -61,8 +63,10 @@ class DataIngestion():
             df.append(_get_whisper_numbers)
         
     def get_earning_calender(self):
-        df_list = pd.read_html('https://www.nasdaq.com/earnings/earnings-calendar.aspx?date=2018-Aug-06')
+        df_list = pd.read_html('https://www.nasdaq.com/earnings/earnings-calendar.aspx?date=2018-Aug-20')
         market_exp = {'M': 1e6, 'B': 1e9}
+        unused_cols = ['sym_mc_size', 'Time', 'multiplier', 'mc_obj', 'eps_consensus_revenue', 'rev1', 'eps', 'consensus']
+        float_cols = ['eps', 'consensus']
         rename = {'CompanyName(Symbol)MarketCapSortby:Name/Size': 'sym_mc_size',
                   'ExpectedReportDate': 'expected_date',
                   "LastYear'sReportDate": 'last_yr_report_date',
@@ -72,19 +76,41 @@ class DataIngestion():
                   '#ofEsts': 'analysts',
                   'FiscalQuarterEnding': 'quarter_ending'
                   }
+        
         df = df_list[0]
         df.columns = df.columns.str.replace('\t', '').str.replace('\n', '').str.replace(' ', '')
         df = df.rename(columns=rename)
-        import pdb; pdb.set_trace()
-
+        df = df.loc[1:].reset_index(drop=True)
         df['ticker'] = df['sym_mc_size'].str.extract('.*\((.*)\).*')
         df['mc_obj'] = df['sym_mc_size'].str.split('$').str.get(1).str[:-1].astype(float)
         df['multiplier'] = df['sym_mc_size'].str.extract('\d+\.\d+(\w)')
         df['market_cap'] = df['mc_obj'].mul(df['multiplier'].map(market_exp))
+        df = df[df['ticker'].notnull()]
+
+        df['eps_consensus_revenue'] = df['ticker'].map(self.get_whisper_numbers)
+        df[['eps', 'consensus', 'revenue']] = pd.DataFrame(df['eps_consensus_revenue'].values.tolist(), index=df.index)
         
-        df = df[df['Symbol'].notnull()]
+        # Revenue cleaning
+        df[['rev1', 'multiplier']] = df['revenue'].str.extract(r'(\d+\.\d+ (\w))')[0].str.split(' ', n=1, expand=True)
+        df['revenue'] = (df['rev1'].astype(float) * df['multiplier'].map(market_exp)).fillna(-1)
+
+        # Consensus cleaning
+        df['consensus_eps'] = df['consensus'].replace('[\$,)]','', regex=True).replace('[(]','-', regex=True).replace('(Consensus: *)', '', regex=True).astype(float)
+
+        # EPS cleaning        
+        df['whisper_eps'] = df['eps'].str.extract(r'(\d+\.\d+)').astype(float)
+        df = df.drop(unused_cols, axis=1)
+        import pdb; pdb.set_trace()        
         return df
-    
+
+    def get_whisper_numbers(self, ticker):
+        r = requests.get(self.EW + ticker)
+        soup = bs(r.text, "html5lib")
+        earnings_per_share = soup.find_all("div", class_="mainitem")[0].get_text().strip()
+        consensus = soup.find_all("div", id="consensus")[0].get_text().strip()
+        revenue = soup.find_all("div", id="revest")[0].get_text().strip()
+        return [earnings_per_share, consensus, revenue]
+
     def normalize():
         pass
 
